3b4998d1463ba168eed6268880cba9e772e70f7e
minhty-fresh <4746495+mdaole@users.noreply.github.com>
2025-05-05 11:25:41 -0700

    remove stick drops from leaves, remove seed drops from grass, remove mob head drops, clean up bamboo stick recipe

--- kubejs/server_scripts/loot_adjustments.js
+++ kubejs/server_scripts/loot_adjustments.js
@@ -1,6 +1,28 @@
 LootJS.modifiers((event) => {
   // remove sheep drops - only shearing to get wool
-  event
-      .addEntityLootModifier("minecraft:sheep")
-      .removeLoot(Ingredient.all);
+  event.addEntityLootModifier("minecraft:sheep").removeLoot(Ingredient.all);
+  
+  // remove seed drops from grass - only tilling to get seeds
+  event.addBlockLootModifier("minecraft:grass").removeLoot("minecraft:wheat_seeds");
+  event.addBlockLootModifier("minecraft:tall_grass").removeLoot("minecraft:wheat_seeds");
+  event.addBlockLootModifier("minecraft:fern").removeLoot("minecraft:wheat_seeds");
+  event.addBlockLootModifier("minecraft:large_fern").removeLoot("minecraft:wheat_seeds");
+
+  // remove stick drops from leaves
+  event.addBlockLootModifier("minecraft:acacia_leaves").removeLoot("minecraft:stick");
+  event.addBlockLootModifier("minecraft:azalea_leaves").removeLoot("minecraft:stick");
+  event.addBlockLootModifier("minecraft:birch_leaves").removeLoot("minecraft:stick");
+  event.addBlockLootModifier("minecraft:cherry_leaves").removeLoot("minecraft:stick");
+  event.addBlockLootModifier("minecraft:dark_oak_leaves").removeLoot("minecraft:stick");
+  event.addBlockLootModifier("minecraft:flowering_azalea_leaves").removeLoot("minecraft:stick");
+  event.addBlockLootModifier("minecraft:jungle_leaves").removeLoot("minecraft:stick");
+  event.addBlockLootModifier("minecraft:mangrove_leaves").removeLoot("minecraft:stick");
+  event.addBlockLootModifier("minecraft:oak_leaves").removeLoot("minecraft:stick");
+  event.addBlockLootModifier("minecraft:spruce_leaves").removeLoot("minecraft:stick");
+
+  // disable mob head drops
+  event.disableWitherStarDrop();
+  event.disableCreeperHeadDrop();
+  event.disableSkeletonHeadDrop();
+  event.disableZombieHeadDrop();
 });
