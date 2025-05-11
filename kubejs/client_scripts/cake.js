447a5c0362843970ba12d5f96b23addf69e10920
minhty-fresh <4746495+mdaole@users.noreply.github.com>
2025-05-10 23:57:16 -0700

    disable cake feature temporarily, it's breaking right click interactions

--- kubejs/client_scripts/cake.js
+++ kubejs/client_scripts/cake.js
@@ -1,26 +1,7 @@
-BlockEvents.rightClicked('minecraft:cake', overEatCake);
-BlockEvents.rightClicked('amendments:double_cake', overEatCake);
+// BlockEvents.rightClicked('minecraft:cake', overEatCake);
+// BlockEvents.rightClicked('amendments:double_cake', overEatCake);
 
-function overEatCake(event) {
-  const { player, level, block } = event;
-  // Same checks as cake.js in server_scripts
-  // Check if the player is not sneaking and has full health and absorption less than 4 hearts)
-  let absorptionHearts = player.getAbsorptionAmount();
-  // allow player to eat to just refresh the duration of absorption
-  let absorptionCanRefresh = (player.hasEffect('absorption')
-    && player.getEffect('absorption').duration < (24000 - 1200) // one minute before the player can eat again to refresh absorption
-    && absorptionHearts == 8 ) // if player has more absorption hearts from other source, cannot eat
-  if (!player.isCrouching() 
-    && !player.canEat(false)
-    && player.health == player.maxHealth
-    && ( absorptionHearts < 8 || absorptionCanRefresh)
-  ) {
-    // make sure that blocks in hand don't get placed when eating cake in this custom function
-    event.success()
-  }
-}
-
-// BlockEvents.rightClicked('minecraft:cake', event => {
+// function overEatCake(event) {
 //   const { player, level, block } = event;
 //   // Same checks as cake.js in server_scripts
 //   // Check if the player is not sneaking and has full health and absorption less than 4 hearts)
@@ -37,5 +18,24 @@ function overEatCake(event) {
 //     // make sure that blocks in hand don't get placed when eating cake in this custom function
 //     event.success()
 //   }
+// }
 
-// });
+// // BlockEvents.rightClicked('minecraft:cake', event => {
+// //   const { player, level, block } = event;
+// //   // Same checks as cake.js in server_scripts
+// //   // Check if the player is not sneaking and has full health and absorption less than 4 hearts)
+// //   let absorptionHearts = player.getAbsorptionAmount();
+// //   // allow player to eat to just refresh the duration of absorption
+// //   let absorptionCanRefresh = (player.hasEffect('absorption')
+// //     && player.getEffect('absorption').duration < (24000 - 1200) // one minute before the player can eat again to refresh absorption
+// //     && absorptionHearts == 8 ) // if player has more absorption hearts from other source, cannot eat
+// //   if (!player.isCrouching() 
+// //     && !player.canEat(false)
+// //     && player.health == player.maxHealth
+// //     && ( absorptionHearts < 8 || absorptionCanRefresh)
+// //   ) {
+// //     // make sure that blocks in hand don't get placed when eating cake in this custom function
+// //     event.success()
+// //   }
+
+// // });
